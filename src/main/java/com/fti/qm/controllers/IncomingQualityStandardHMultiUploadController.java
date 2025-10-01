38cfddc4e503b43ee5f37a3f7560e9dec10f4405
crew52 <dungnc150602@gmail.com>
2025-10-01 10:23:30 +0700

    feat: done upload file

--- src/main/java/com/fti/qm/controllers/IncomingQualityStandardHMultiUploadController.java
+++ src/main/java/com/fti/qm/controllers/IncomingQualityStandardHMultiUploadController.java
@@ -1,18 +1,11 @@
-
 package com.fti.qm.controllers;
 
 import com.google.common.io.Files;
+import com.qcadoo.model.api.*;
+import com.qcadoo.model.api.file.FileService;
 import com.fti.qm.constants.QMConstants;
 import com.fti.qm.constants.IQSHAttachmentFields;
-import com.qcadoo.model.api.DataDefinition;
-import com.qcadoo.model.api.DataDefinitionService;
-import com.qcadoo.model.api.Entity;
-import com.qcadoo.model.api.NumberService;
-import com.qcadoo.model.api.file.FileService;
-import com.qcadoo.mes.basic.MultiUploadHelper;
 import org.apache.commons.io.IOUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -27,10 +20,7 @@ import java.util.Iterator;
 
 @Controller
 @RequestMapping("/qm")
-public class QmMultiUploadController {
-
-    private static final Logger logger = LoggerFactory.getLogger(QmMultiUploadController.class);
-    private static final int L_SCALE = 2;
+public class IncomingQualityStandardHMultiUploadController {
 
     @Autowired
     private FileService fileService;
@@ -41,52 +31,43 @@ public class QmMultiUploadController {
     @Autowired
     private NumberService numberService;
 
+    private static final Integer L_SCALE = 2;
+
     @ResponseBody
     @RequestMapping(value = "/multiUploadFiles", method = RequestMethod.POST)
-    public void upload(MultipartHttpServletRequest request, HttpServletResponse response) {
-        Long iqshId = Long.parseLong(request.getParameter("techId")); // Changed from iQSHId to techId to match frontend
-        Entity iqsh = dataDefinitionService.get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_INCOMING_QUALITY_STANDARD_H)
-                .get(iqshId);
+    public void upload(final MultipartHttpServletRequest request, final HttpServletResponse response) {
+        Long iQSHId = Long.parseLong(request.getParameter("iQSHId"));
 
-        DataDefinition attachmentDD = dataDefinitionService
-                .get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_IQSH_ATTACHMENT);
+        Entity iQSH = dataDefinitionService.get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_INCOMING_QUALITY_STANDARD_H).get(iQSHId);
+        DataDefinition attachmentDD = dataDefinitionService.get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_IQSH_ATTACHMENT);
 
         Iterator<String> itr = request.getFileNames();
-
         while (itr.hasNext()) {
             MultipartFile mpf = request.getFile(itr.next());
-            String path = "";
 
             try {
-                path = fileService.upload(mpf);
-            } catch (IOException e) {
-                logger.error("Unable to upload attachment.", e);
-                continue;
-            }
-
-            if (MultiUploadHelper.EXTS.contains(Files.getFileExtension(path).toUpperCase())) {
+                String path = fileService.upload(mpf);
                 Entity attachment = attachmentDD.create();
                 attachment.setField(IQSHAttachmentFields.ATTACHMENT, path);
                 attachment.setField(IQSHAttachmentFields.NAME, mpf.getOriginalFilename());
-                attachment.setField(IQSHAttachmentFields.INCOMING_QUALITY_STANDARD_H, iqsh);
+                attachment.setField(IQSHAttachmentFields.INCOMING_QUALITY_STANDARD_H, iQSH);
                 attachment.setField(IQSHAttachmentFields.EXT, Files.getFileExtension(path));
 
                 BigDecimal fileSize = new BigDecimal(mpf.getSize(), numberService.getMathContext());
-                BigDecimal divider = new BigDecimal(1024, numberService.getMathContext());
-                BigDecimal size = fileSize.divide(divider, L_SCALE, BigDecimal.ROUND_HALF_UP);
-
+                BigDecimal size = fileSize.divide(new BigDecimal(1024), L_SCALE, BigDecimal.ROUND_HALF_UP);
                 attachment.setField(IQSHAttachmentFields.SIZE, size);
+
                 attachmentDD.save(attachment);
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
     @RequestMapping(value = "/getAttachment.html", method = RequestMethod.GET)
-    public void getAttachment(@RequestParam("id") final Long[] ids, HttpServletResponse response) {
-        DataDefinition attachmentDD = dataDefinitionService
-                .get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_IQSH_ATTACHMENT);
+    public final void getAttachment(@RequestParam("id") final Long[] ids, final HttpServletResponse response) {
+        DataDefinition attachmentDD = dataDefinitionService.get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_IQSH_ATTACHMENT);
         Entity attachment = attachmentDD.get(ids[0]);
-
         InputStream is = fileService.getInputStream(attachment.getStringField(IQSHAttachmentFields.ATTACHMENT));
 
         try {
@@ -95,17 +76,13 @@ public class QmMultiUploadController {
                 return;
             }
 
-            response.setHeader("Content-disposition",
-                    "inline; filename=" + attachment.getStringField(IQSHAttachmentFields.NAME));
-            response.setContentType(
-                    fileService.getContentType(attachment.getStringField(IQSHAttachmentFields.ATTACHMENT)));
-
+            response.setHeader("Content-disposition", "inline; filename=" + attachment.getStringField(IQSHAttachmentFields.NAME));
+            response.setContentType(fileService.getContentType(attachment.getStringField(IQSHAttachmentFields.ATTACHMENT)));
             int bytes = IOUtils.copy(is, response.getOutputStream());
             response.setContentLength(bytes);
-
             response.flushBuffer();
         } catch (IOException e) {
-            logger.error("Unable to copy attachment file to response stream.", e);
+            e.printStackTrace();
         }
     }
 }
--- target/classes/qm/public/js/multiuploadfunctions.js
+++ target/classes/qm/public/js/multiuploadfunctions.js
@@ -1,106 +1,49 @@
-$(function () {
+$(function() {
     $('#fileupload').fileupload({
         pasteZone: null,
         dataType: 'json',
         acceptFileTypes: QCDMultiUpload.acceptFileTypes,
 
-        submit: function (e, data) {
-            const locale = window.mainController
-                .getComponentByReferenceName("iQSHMultiUploadLocale")
-                .getValue().content.value;
+        submit: function(e, data) {
+            var locale = window.mainController.getComponentByReferenceName("iQSHMultiUploadLocale").getValue().content.value;
+            var idField = window.mainController.getComponentByReferenceName("iQSHIdForMultiUpload").getValue();
+            var idValue = idField.content;
 
-            const entityIdComp = window.mainController
-                .getComponentByReferenceName("iQSHIdForMultiUpload")
-                .getValue();
-
-            const entityId = entityIdComp.content;
-
-            if (!entityId.value || entityId.value === 0) {
-                $.each(data.files, function (index, file) {
-                    if (locale === "pl_PL" || locale === "pl") {
-                        showMessage("failure", "Obiekt niezapisany", "Pominięto wgranie pliku: " + file.name);
-                    } else {
-                        showMessage("failure", "Entity not saved", "Omitted file upload: " + file.name);
-                    }
+            if (!idValue.value || 0 === idValue.value) {
+                $.each(data.files, function(index, file) {
+                    showMessage("failure", "IncomingQualityStandardH is not saved", "Omitted file upload: " + file.name);
                 });
                 return false;
             }
         },
 
-        done: function (e, data) {
-            const mainViewComponent =
-                window.mainController.getComponentByReferenceName("form") ||
-                window.mainController.getComponentByReferenceName("grid");
+        done: function(e, data) {
+            var form = mainController.getComponentByReferenceName("form");
+            if (form) form.performRefresh();
 
-            if (mainViewComponent) {
-                mainViewComponent.performRefresh();
-            }
-
-            const locale = window.mainController
-                .getComponentByReferenceName("iQSHMultiUploadLocale")
-                .getValue().content.value;
-
-            $.each(data.files, function (index, file) {
-                if (QCDMultiUpload.acceptFileTypes.test(file.name)) {
-                    if (locale === "pl_PL" || locale === "pl") {
-                        showMessage("success", "Wgrywanie zakończone", "Wgrano plik: " + file.name);
-                    } else {
-                        showMessage("success", "Upload complete", "Uploaded file: " + file.name);
-                    }
-                }
+            var locale = window.mainController.getComponentByReferenceName("iQSHMultiUploadLocale").getValue().content.value;
+            $.each(data.files, function(index, file) {
+                showMessage("success", "Upload complete", "Loaded file: " + file.name);
             });
         },
 
-        progressall: function (e, data) {
-            const progress = parseInt(data.loaded / data.total * 100, 10);
+        progressall: function(e, data) {
+            var progress = parseInt(data.loaded / data.total * 100, 10);
             $('#progress .progress-bar').css('width', progress + '%');
         },
 
         dropZone: $('#dropzone')
-    })
-    .bind('fileuploadsubmit', function (e, data) {
-        const entityIdComp = window.mainController
-            .getComponentByReferenceName("iQSHIdForMultiUpload")
-            .getValue();
-
-        const entityId = entityIdComp.content;
+    }).bind('fileuploadsubmit', function(e, data) {
+        var idField = window.mainController.getComponentByReferenceName("iQSHIdForMultiUpload").getValue();
+        var idValue = idField.content;
         data.formData = {
-            techId: entityId.value   // vẫn dùng tên 'techId' để backend hiểu
+            iQSHId: idValue.value
         };
-    })
-    .bind('fileuploadadd', function (e, data) {
-        const filetype = QCDMultiUpload.acceptFileTypes;
-        const maxUploadFileMessage = $('#maxUploadFileMessage').text();
-        const maxSize = parseInt(maxUploadFileMessage.replace(/^\D+/g, '')); // lấy số MB
-
-        const locale = window.mainController
-            .getComponentByReferenceName("iQSHMultiUploadLocale")
-            .getValue().content.value;
-
-        $.each(data.files, function (index, file) {
-            if (!filetype.test(file.name)) {
-                if (locale === "pl_PL" || locale === "pl") {
-                    showMessage("failure", "Pominięto wgranie pliku", "Niedopuszczalny typ pliku: " + file.name);
-                } else {
-                    showMessage("failure", "Upload skipped", "Invalid file type: " + file.name);
-                }
-                return false;
-            }
-
-            if (file.size / 1048576 > maxSize) {
-                if (locale === "pl_PL" || locale === "pl") {
-                    showMessage("failure", "Pominięto wgranie pliku", "Plik: " + file.name + " " + maxUploadFileMessage);
-                } else {
-                    showMessage("failure", "Upload skipped", "File: " + file.name + " " + maxUploadFileMessage);
-                }
-                return false;
-            }
-        });
     });
 });
 
 function showMessage(type, title, content) {
-    window.mainController.showMessage({
+    mainController.showMessage({
         type: type,
         title: title,
         content: content
