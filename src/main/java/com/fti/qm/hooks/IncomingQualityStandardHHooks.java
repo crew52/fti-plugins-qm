e4c638550781f0e0a84e2c365ebbb2977429084c
crew52 <dungnc150602@gmail.com>
2025-10-06 12:31:45 +0700

    feat: Hearder List them cot status -> da co bo tieu chuan

--- src/main/java/com/fti/qm/hooks/IncomingQualityStandardHHooks.java
+++ src/main/java/com/fti/qm/hooks/IncomingQualityStandardHHooks.java
@@ -4,8 +4,13 @@ import com.fti.qm.constants.IncomingQualityStandardHFields;
 import com.qcadoo.model.api.*;
 import com.qcadoo.model.api.search.SearchCriteriaBuilder;
 import com.qcadoo.model.api.search.SearchRestrictions;
+import com.qcadoo.view.api.ViewDefinitionState;
+import com.qcadoo.view.api.components.GridComponent;
+import com.qcadoo.view.constants.QcadooViewConstants;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Service
 public class IncomingQualityStandardHHooks {
@@ -35,5 +40,27 @@ public class IncomingQualityStandardHHooks {
             incomingStandardH.addError(dataDefinition.getField(IncomingQualityStandardHFields.PRODUCT),
                     "qm.message.error.productAlreadyExistsWithActiveStandard");
         }
+    }
+
+    public void setStatusText(final ViewDefinitionState view) {
+        GridComponent grid = (GridComponent) view.getComponentByReference(QcadooViewConstants.L_GRID);
+
+        if (grid == null) {
+            return;
+        }
+
+        List<Entity> entities = grid.getEntities();
+
+        for (Entity entity : entities) {
+            List<Entity> lines = entity.getHasManyField(IncomingQualityStandardHFields.INCOMING_QUALITY_STANDARD_LS);
+
+            String status = (lines == null || lines.isEmpty())
+                    ? IncomingQualityStandardHFields.STATUS_NO_STANDARD
+                    : IncomingQualityStandardHFields.STATUS_HAS_STANDARD;
+
+            entity.setField(IncomingQualityStandardHFields.STATUS_TEXT, status);
+        }
+
+        grid.setEntities(entities);
     }
 }
