e491f4a02a8770dfe78d045587025a62a9e975bf
crew52 <dungnc150602@gmail.com>
2025-10-08 16:52:08 +0700

    feat: done import

---
+++ src/main/java/com/fti/qm/imports/qualityCriteria/QualityCriteriaXlsxImportService.java
@@ -0,0 +1,8 @@
+package com.fti.qm.imports.qualityCriteria;
+
+import com.qcadoo.mes.basic.imports.services.XlsxImportService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QualityCriteriaXlsxImportService extends XlsxImportService {
+}
