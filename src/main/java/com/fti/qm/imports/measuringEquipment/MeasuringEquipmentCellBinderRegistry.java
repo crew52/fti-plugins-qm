4f294cc5e2a2a31413c5b98465ed2e54835d641f
crew52 <dungnc150602@gmail.com>
2025-10-08 16:02:58 +0700

    feat: done show log

---
+++ src/main/java/com/fti/qm/imports/measuringEquipment/MeasuringEquipmentCellBinderRegistry.java
@@ -0,0 +1,23 @@
+package com.fti.qm.imports.measuringEquipment;
+
+import com.fti.qm.constants.MeasuringEquipmentFields;
+import org.springframework.stereotype.Component;
+import com.qcadoo.mes.basic.imports.dtos.CellBinderRegistry;
+import static com.qcadoo.mes.basic.imports.dtos.CellBinder.required;
+import javax.annotation.PostConstruct;
+
+@Component
+public class MeasuringEquipmentCellBinderRegistry {
+    private CellBinderRegistry cellBinderRegistry = new CellBinderRegistry();
+
+    @PostConstruct
+    private void init() {
+        cellBinderRegistry.setCellBinder(required(MeasuringEquipmentFields.NUMBER));
+        cellBinderRegistry.setCellBinder(required(MeasuringEquipmentFields.NAME));
+        cellBinderRegistry.setCellBinder(required(MeasuringEquipmentFields.MEASURING_METHOD));
+    }
+
+    public CellBinderRegistry getCellBinderRegistry() {
+        return cellBinderRegistry;
+    }
+}
--- src/main/java/com/fti/qm/listeners/MeasuringEquipmentsImportListeners.java
+++ src/main/java/com/fti/qm/listeners/MeasuringEquipmentsImportListeners.java
@@ -1,20 +1,48 @@
 package com.fti.qm.listeners;
 
+import com.fti.qm.constants.MeasuringEquipmentFields;
 import com.fti.qm.constants.QMConstants;
+import com.fti.qm.imports.measuringEquipment.MeasuringEquipmentCellBinderRegistry;
 import com.fti.qm.imports.measuringEquipment.MeasuringEquipmentXlsxImportService;
 import com.qcadoo.mes.basic.imports.services.XlsxImportService;
+import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchCriterion;
+import com.qcadoo.model.api.search.SearchRestrictions;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class MeasuringEquipmentsImportListeners {
     @Autowired
     private MeasuringEquipmentXlsxImportService measuringEquipmentXlsxImportService;
 
+    @Autowired
+    private MeasuringEquipmentCellBinderRegistry measuringEquipmentCellBinderRegistry;
+
+    public void processImportFile(final ViewDefinitionState view, final ComponentState state, final String[] args)
+            throws IOException {
+        measuringEquipmentXlsxImportService.processImportFile(view, measuringEquipmentCellBinderRegistry.getCellBinderRegistry(), true,
+                QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_MEASURING_EQUIPMENT, MeasuringEquipmentsImportListeners::createRestrictionForMeasuringEquipment);
+    }
+
+    private static SearchCriterion createRestrictionForMeasuringEquipment(final Entity measuringEquipment) {
+        return SearchRestrictions.eq(MeasuringEquipmentFields.NUMBER, measuringEquipment.getStringField(MeasuringEquipmentFields.NUMBER));
+    }
+
     public void downloadImportSchema(final ViewDefinitionState view, final ComponentState state, final String[] args) {
         measuringEquipmentXlsxImportService.downloadImportSchema(view, QMConstants.PLUGIN_IDENTIFIER,
                 QMConstants.MODEL_MEASURING_EQUIPMENT, XlsxImportService.L_XLSX);
     }
+    
+    public void redirectToLogs(final ViewDefinitionState view, final ComponentState state, final String[] args) {
+        measuringEquipmentXlsxImportService.redirectToLogs(view, QMConstants.MODEL_MEASURING_EQUIPMENT);
+    }
+
+     public void onInputChange(final ViewDefinitionState view, final ComponentState state, final String[] args) {
+         measuringEquipmentXlsxImportService.changeButtonsState(view, false);
+     }
 }
