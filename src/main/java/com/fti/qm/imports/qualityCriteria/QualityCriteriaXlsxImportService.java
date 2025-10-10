aef2d79fa0a79b347c79fbfbc1f9a24fdcdd44b7
crew52 <dungnc150602@gmail.com>
2025-10-10 15:51:35 +0700

    feat: done tieu chi import

--- src/main/java/com/fti/qm/imports/qualityCriteria/QualityCriteriaXlsxImportService.java
+++ src/main/java/com/fti/qm/imports/qualityCriteria/QualityCriteriaXlsxImportService.java
@@ -1,8 +1,25 @@
 package com.fti.qm.imports.qualityCriteria;
 
+import com.fti.qm.constants.QualityCriteriaFields;
 import com.qcadoo.mes.basic.imports.services.XlsxImportService;
+import com.qcadoo.model.api.DataDefinition;
+import com.qcadoo.model.api.Entity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QualityCriteriaXlsxImportService extends XlsxImportService {
+    private static final String L_QCADOO_VIEW_VALIDATE_FIELD_ERROR_CUSTOM =
+            "qcadooView.validate.field.error.custom";
+
+    @Override
+    public void validateEntity(final Entity criteria, final DataDefinition criteriaDD) {
+        // Nếu type không null thì phải là 01qualitative hoặc 02quantitative
+        Object type = criteria.getField(QualityCriteriaFields.TYPE);
+
+        if (type != null &&
+                !(type.equals("01qualitative") || type.equals("02quantitative"))) {
+            criteria.addError(criteriaDD.getField(QualityCriteriaFields.TYPE),
+                    L_QCADOO_VIEW_VALIDATE_FIELD_ERROR_CUSTOM);
+        }
+    }
 }
