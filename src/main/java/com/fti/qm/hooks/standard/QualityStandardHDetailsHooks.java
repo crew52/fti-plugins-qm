31ee6350ebca9f7d918d8627e1a1675a4ef6dc93
crew52 <dungnc150602@gmail.com>
2025-11-26 16:47:19 +0700

    feat: add version of qic

--- src/main/java/com/fti/qm/hooks/standard/QualityStandardHDetailsHooks.java
+++ src/main/java/com/fti/qm/hooks/standard/QualityStandardHDetailsHooks.java
@@ -7,7 +7,6 @@ import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
 import com.qcadoo.model.api.search.SearchRestrictions;
-import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
 import com.qcadoo.view.api.components.FormComponent;
 import com.qcadoo.view.api.components.WindowComponent;
--- src/main/java/com/fti/qm/listeners/standard/QualityStandardHVersionListener.java
+++ src/main/java/com/fti/qm/listeners/standard/QualityStandardHVersionListener.java
@@ -5,6 +5,7 @@ import com.fti.qm.constants.qualityInspectionCommand.QICFields;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.DataDefinitionService;
 import com.qcadoo.model.api.Entity;
+import com.qcadoo.model.api.search.SearchOrders;
 import com.qcadoo.model.api.search.SearchRestrictions;
 import com.qcadoo.view.api.ComponentState;
 import com.qcadoo.view.api.ViewDefinitionState;
@@ -12,6 +13,7 @@ import com.qcadoo.view.api.components.FormComponent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -39,19 +41,60 @@ public class QualityStandardHVersionListener {
             return;
         }
 
-        boolean exists = qicExistsForProductIncoming(product.getId());
+        DataDefinition qicDD = dataDefinitionService.get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_QUALITY_INSPECTION_COMMAND);
+        if (qicDD == null) {
+            view.addMessage("qm.qualityStandardH.error.cannotAccessQIC", ComponentState.MessageType.FAILURE);
+            return;
+        }
 
-        if (!exists) {
-            // TH1: Không tìm thấy QIC cho product + 01incoming
+        // Lấy QIC cũ (product + 01incoming)
+        Entity oldQIC = qicDD.find()
+                .add(SearchRestrictions.eq("product.id", product.getId()))
+                .add(SearchRestrictions.eq(QICFields.INSPECTION_TYPE, QICFields.INSPECTION_TYPE_INCOMING))
+                .addOrder(SearchOrders.desc("version"))
+                .setMaxResults(1)
+                .uniqueResult();
+
+        if (oldQIC == null) {
+            // TH1: Không tìm thấy QIC
             view.addMessage("qm.qualityStandardH.info.qicNotFound", ComponentState.MessageType.INFO);
             return;
-        } else {
-            // TH2: Tìm thấy QIC
-            view.addMessage("qm.qualityStandardH.info.qicFound", ComponentState.MessageType.SUCCESS);
-
-            // Bạn sẽ triển khai bước tiếp theo ở đây sau
-            // qualityStandardVersionService.createNewVersion(qsh);
         }
+
+        // TH2: Tìm thấy QIC -> tạo QIC mới dựa trên QIC cũ
+        Entity newQIC = qicDD.create();
+
+        // Copy dữ liệu từ QIC cũ
+        newQIC.setField("inspectionType", oldQIC.getField("inspectionType"));
+        newQIC.setField("status", "01new"); // trạng thái mới
+        newQIC.setField("productionOrderNumber", oldQIC.getField("productionOrderNumber"));
+        newQIC.setField("operationNumber", oldQIC.getField("operationNumber"));
+        newQIC.setField("company", oldQIC.getField("company"));
+        newQIC.setField("product", oldQIC.getField("product"));
+        newQIC.setField("tool", oldQIC.getField("tool"));
+        newQIC.setField("inspectionOrderNumber", oldQIC.getField("inspectionOrderNumber"));
+        newQIC.setField("inspectionOrderNumberInt", oldQIC.getField("inspectionOrderNumberInt"));
+        newQIC.setField("poNumber", oldQIC.getField("poNumber"));
+        newQIC.setField("executionDate", oldQIC.getField("executionDate"));
+        newQIC.setField("transactionQuantity", oldQIC.getField("transactionQuantity"));
+        newQIC.setField("location", oldQIC.getField("location"));
+        newQIC.setField("qualityDecision", oldQIC.getField("qualityDecision"));
+        newQIC.setField("warehouseQuantity", oldQIC.getField("warehouseQuantity"));
+        newQIC.setField("ngQuantity", oldQIC.getField("ngQuantity"));
+        newQIC.setField("warehouseLocation", oldQIC.getField("warehouseLocation"));
+        newQIC.setField("ngLocation", oldQIC.getField("ngLocation"));
+
+        // Version mới = version cũ + 1
+        Integer oldVersion = oldQIC.getIntegerField("version");
+        newQIC.setField("version", oldVersion != null ? oldVersion + 1 : 1);
+
+        // Ngày tạo mới
+        newQIC.setField("createdDate", new Date());
+
+        // Lưu QIC mới
+        qicDD.save(newQIC);
+
+        view.addMessage("qm.qualityStandardH.info.newQicCreated", ComponentState.MessageType.SUCCESS);
     }
 
     /**
@@ -60,7 +103,7 @@ public class QualityStandardHVersionListener {
     private boolean qicExistsForProductIncoming(final Long productId) {
         if (productId == null) return false;
 
-        DataDefinition qicDD = dataDefinitionService.get("qm", "qualityInspectionCommandRe");
+        DataDefinition qicDD = dataDefinitionService.get(QMConstants.PLUGIN_IDENTIFIER, QMConstants.MODEL_QUALITY_INSPECTION_COMMAND);
         if (qicDD == null) return false;
 
         List<?> qics = qicDD.find()
