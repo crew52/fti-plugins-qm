3d217d5e6102c5eb38a443dbf4670da9df244f9b
crew52 <dungnc150602@gmail.com>
2025-11-27 16:11:01 +0700

    add soft delete of Line -> Sample

--- src/main/java/com/fti/qm/hooks/standard/QualityStandardLHooks.java
+++ src/main/java/com/fti/qm/hooks/standard/QualityStandardLHooks.java
@@ -1,13 +1,21 @@
 package com.fti.qm.hooks.standard;
 
+import com.fti.qm.constants.GlobalFields;
+import com.fti.qm.constants.QMConstants;
 import com.fti.qm.constants.QSLFields;
+import com.fti.qm.helpers.SoftDeleteHelper;
 import com.fti.qm.hooks.standard.base.BaseQualityStandardLHooks;
 import com.qcadoo.model.api.DataDefinition;
 import com.qcadoo.model.api.Entity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QualityStandardLHooks extends BaseQualityStandardLHooks {
+
+    @Autowired
+    private SoftDeleteHelper softDeleteHelper;
+
     @Override protected String getQualitativeCheckboxField() { return QSLFields.QUALITATIVE_CHECKBOX; }
     @Override protected String getQualitativeValueField()     { return QSLFields.QUALITATIVE_VALUE; }
     @Override protected String getQualitativePassConstant()   { return QSLFields.QualitativeValue.PASS; }
@@ -20,5 +28,16 @@ public class QualityStandardLHooks extends BaseQualityStandardLHooks {
 
     public void onView(final DataDefinition dd, final Entity entity) {
         super.onView(entity);
+    }
+
+    public void onUpdate(final DataDefinition dataDefinition, final Entity qualityStandardL) {
+        Boolean deleted = qualityStandardL.getBooleanField(GlobalFields.DELETED);
+        if (deleted == null || !deleted) {
+            return;
+        }
+
+        // Xóa mềm các bản ghi con
+        String joinField = QMConstants.MODEL_QUALITY_STANDARD_L;
+        softDeleteHelper.softDeleteRelatedLines(QMConstants.MODEL_QUALITY_STANDARD_SAMPLE, joinField, qualityStandardL);
     }
 }
