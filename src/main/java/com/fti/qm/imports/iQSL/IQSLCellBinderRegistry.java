0cf6d4df4f49bc9f78a01943d090d125f9b77309
crew52 <dungnc150602@gmail.com>
2025-12-05 14:05:40 +0700

    feat: add new qSLQualitativeValueCellParser

--- src/main/java/com/fti/qm/imports/iQSL/IQSLCellBinderRegistry.java
+++ src/main/java/com/fti/qm/imports/iQSL/IQSLCellBinderRegistry.java
@@ -28,6 +28,10 @@ public class IQSLCellBinderRegistry {
     @Autowired
     private CellParser measuringEquipmentCellParser;
 
+    @Autowired
+    @Qualifier("qslQualitativeValueCellParser")
+    private CellParser qSLQualitativeValueCellParser;
+
     @PostConstruct
     private void init() {
         cellBinderRegistry.setCellBinder(required("qualityStandardH", qshIncomingLookupByProductNumberParser));
@@ -36,7 +40,7 @@ public class IQSLCellBinderRegistry {
         cellBinderRegistry.setCellBinder(required("sampleSize"));
         cellBinderRegistry.setCellBinder(optional("position"));
         cellBinderRegistry.setCellBinder(optional("description"));
-        cellBinderRegistry.setCellBinder(optional("qualitativeValue"));
+        cellBinderRegistry.setCellBinder(optional("qualitativeValue", qSLQualitativeValueCellParser));
         cellBinderRegistry.setCellBinder(optional("quantitativeValue"));
         cellBinderRegistry.setCellBinder(optional("tolerance"));
         cellBinderRegistry.setCellBinder(optional("upValue"));
