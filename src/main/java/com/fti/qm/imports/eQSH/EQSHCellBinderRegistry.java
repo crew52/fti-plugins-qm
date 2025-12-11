85297bdc5c7dc1800406dc45bbb59c9cdd05deeb
crew52 <dungnc150602@gmail.com>
2025-12-11 14:10:48 +0700

    feat: done parse loai thiet bi

--- src/main/java/com/fti/qm/imports/eQSH/EQSHCellBinderRegistry.java
+++ src/main/java/com/fti/qm/imports/eQSH/EQSHCellBinderRegistry.java
@@ -4,6 +4,7 @@ import com.fti.qm.constants.QSHFields;
 import com.qcadoo.mes.basic.imports.dtos.CellBinderRegistry;
 import com.qcadoo.mes.basic.imports.helpers.CellParser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -20,10 +21,14 @@ public class EQSHCellBinderRegistry {
     @Autowired
     private CellParser toolCellParser;
 
+    @Autowired
+    @Qualifier("eqshInspectionTypeCellParser")
+    private CellParser eqshInspectionTypeCellParser;
+
     @PostConstruct
     private void init() {
         cellBinderRegistry.setCellBinder(required(QSHFields.TOOL, toolCellParser));
-        cellBinderRegistry.setCellBinder(required(QSHFields.INSPECTION_TYPE));
+        cellBinderRegistry.setCellBinder(required(QSHFields.INSPECTION_TYPE, eqshInspectionTypeCellParser));
     }
 
     public CellBinderRegistry getCellBinderRegistry() {
